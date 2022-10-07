65aab2653fb6bc83755a9e8d43b003ff7f460bec
mantonoiu <antonoiumadalin@gmail.com>
2022-10-07 19:21:42 +0100

    webpack5 polyfill

---
+++ next-app/next.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  webpack: function (config, {webpack}) {
+    console.log(webpack.version);
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      url: require.resolve('url'),
+      fs: require.resolve('fs'),
+      assert: require.resolve('assert'),
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      buffer: require.resolve('buffer'),
+      stream: require.resolve('stream-browserify'),
+    },
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      })
+  )
+    return config;
+  }
+}
